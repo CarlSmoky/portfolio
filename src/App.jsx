4c9221316b41e73cd8b368593e51ffefa754e7d6
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-24 19:33:03 -0400

    Change App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import Projects from './components/Projects';
 
 export default function App() {
   
-  return (<div className="App" >
-    <h1> portfolio </h1>
+  return (
+  <div className="App" >
+    <Header></Header>
+    <Home></Home>
+    <Projects></Projects>
   </div >
   );
 }
