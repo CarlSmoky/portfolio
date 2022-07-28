79f125925208066e428f17db9edca24f16c193fc
tsumitakaoru <k.tsumita01@gmail.com>
2022-07-28 14:43:30 -0400

    Add sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import "./sass/main.scss"
 import Header from './components/Header';
 import Home from './components/Home';
+import About from "./components/About";
+import Skill from "./components/Skill";
 import Projects from './components/Projects';
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
 
 export default function App() {
   
   return (
   <div className="App" >
     <Header></Header>
-    <Home></Home>
+    <About></About>
+    <Skill></Skill>
     <Projects></Projects>
+    <Contact></Contact>
+    <Footer></Footer>
   </div >
   );
 }
