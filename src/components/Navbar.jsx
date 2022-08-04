da87ccb9176b2f6720a39b8ffcee78d972e92b59
tsumitakaoru <k.tsumita01@gmail.com>
2022-08-04 15:59:26 -0400

    Change icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { AiOutlineHome } from 'react-icons/ai';
 import { AiOutlineUser } from 'react-icons/ai';
-import { BiBook } from 'react-icons/bi';
-import { RiServiceLine } from 'react-icons/ri';
-import { BiMessageSquareDetail } from 'react-icons/bi';
+import { BiCodeAlt } from 'react-icons/bi';
+import { FaLaptopCode } from 'react-icons/fa';
+import { HiOutlineMail } from 'react-icons/hi';
 
 const Navbar = () => {
   const [activeNav, setActiveNav] = useState('#home');
@@ -11,9 +11,9 @@ const Navbar = () => {
     <nav className="nav">
       <a href="#home" onClick={()=> setActiveNav('#home')} className={activeNav === '#home' ? 'active' : ''}><AiOutlineHome /></a>
       <a href="#about" onClick={()=> setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
-      <a href="#skills" onClick={()=> setActiveNav('#skill')} className={activeNav === '#skill' ? 'active' : ''}><BiBook /></a>
-      <a href="#projects" onClick={()=> setActiveNav('#projects')} className={activeNav === '#projects' ? 'active' : ''}><RiServiceLine /></a>
-      <a href="#contact" onClick={()=> setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail /></a>
+      <a href="#skills" onClick={()=> setActiveNav('#skill')} className={activeNav === '#skill' ? 'active' : ''}><BiCodeAlt /></a>
+      <a href="#projects" onClick={()=> setActiveNav('#projects')} className={activeNav === '#projects' ? 'active' : ''}><FaLaptopCode /></a>
+      <a href="#contact" onClick={()=> setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><HiOutlineMail /></a>
     </nav>
   )
 }
