f4a1317252dca7691b7540fae59b57103c0fcef3
tsumitakaoru <k.tsumita01@gmail.com>
2022-08-09 20:38:38 -0400

    Change Header style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,18 @@ import React from "react";
 function Header() {
   return (
     <header className="header" id="home">
-    <div className="header__text-box">
-      <h2>Kaoru Tsumita</h2>
-      <h3>Full Stack Developer</h3>
-    </div>
-  </header>
+      <div className="header__text-box">
+        <div className="fadeup-enter--1">
+          <h1 className="header__name">Kaoru Tsumita</h1>
+        </div>
+        <div className="fadeup-enter--2">
+          <h2 className="header__title">Full Stack Develper</h2>
+        </div>
+        <div className="fadeup-enter--3">
+          <p className="header__text paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores ab sapiente rem nihil atque harum nobis iusto fugit incidunt eligendi amet, asperiores doloremque non dicta aliquid saepe aperiam doloribus eius!</p>
+        </div>
+      </div>
+    </header>
   )
 }
 
