0c1ebf5dd95f91c158545b7a0f1ffd12ac39648a
tsumitakaoru <k.tsumita01@gmail.com>
2022-08-11 19:35:59 -0400

    Add heading

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,6 +4,11 @@ import profilePhoto from '../img/image5.jpg';
 const About = () => {
   return (
     <section className="section-about" id="about" >
+      <div className="u-center-text u-margin-bottom-small">
+        <h2 className="heading-secondary">
+          About
+        </h2>
+      </div>
       <div className="section-about__my-name">
         <img className="section-about__profile_photo" src={profilePhoto} alt="profile_photo" />
         <h3>Kaoru Tsumita</h3>
