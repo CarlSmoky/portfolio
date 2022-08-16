661e6e405e1477ac140f45f0505dffe7a74a8bfc
tsumitakaoru <k.tsumita01@gmail.com>
2022-08-15 22:59:26 -0400

    Change about style

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,12 +6,16 @@ const About = () => {
     <section className="section-about" id="about" >
       <div className="u-center-text u-margin-bottom-small">
         <h2 className="heading-secondary">
-          About
+          About Me
         </h2>
       </div>
-      <div className="section-about__my-name">
-        <img className="section-about__profile_photo" src={profilePhoto} alt="profile_photo" />
-        <h3>Kaoru Tsumita</h3>
+      <div className="about__box">
+        <div className="about__image">
+          <img className="about__image--photo" src={profilePhoto} alt="profile_photo" />
+        </div>
+        <div className="about__text">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde possimus expedita quam error aspernatur, distinctio libero voluptatibus reiciendis fugiat nisi dolores incidunt, alias aliquam? Ex ratione laboriosam amet neque mollitia.</p>
+        </div>
       </div>
     </section>
   )
