9e01b298df1a6e45ea2ffa28acb7bfc524df565f
tsumitakaoru <k.tsumita01@gmail.com>
2022-10-10 11:46:46 -0400

    Change about style

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,12 +4,10 @@ import profilePhoto from '../img/image5.jpg';
 
 const About = () => {
   return (
-    <section className="section-about" id="about" >
-      <div className="u-center-text u-margin-bottom-small">
+    <section className="section-about u-center-text u-margin-bottom-small" id="about" >
         <h2 className="heading-secondary">
           About Me
         </h2>
-      </div>
       <div className="about__box">
         <div className="about__image">
           <img className="about__image--photo" src={profilePhoto} alt="profile_photo" />
@@ -19,7 +17,6 @@ const About = () => {
             <p>I’ve recently transitioned into a career as a full stack developer, and I’m excited by all the possibilities in modern web technology and the opportunities they can provide, and am driven to learn and continuously improve my own skills. Bridging the gap between people and technology to make lives easier is a crucial motivation for me.</p>
           </div>
           <div className="section-skill">
-            {/* <SkillList></SkillList> */}
           </div>
         </div>
       </div>
